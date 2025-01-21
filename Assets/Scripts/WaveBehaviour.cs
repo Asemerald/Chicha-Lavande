8f858e0ea6630ae0591782e83f1705910b2eb37e
EliotTranson <117980205+EliotTranson@users.noreply.github.com>
2025-01-21 12:17:43 +0100

    Shaders V4

--- Assets/Scripts/WaveBehaviour.cs
+++ Assets/Scripts/WaveBehaviour.cs
@@ -7,17 +7,37 @@ public class WaveBehaviour : MonoBehaviour
 
     private float circleScaleValue = 0;
 
+    [SerializeField] private ParticleSystem ps;
+
+    private void Start()
+    {
+        var main = ps.main;
+        main.startSize = audioPower * 0.1f;
+        main.startLifetime = audioPower * 0.05f;
+        
+        //ps.sizeOverLifetime.size.curve[]
+    }
+
     private void Update()
     {
+        
         if (audioPower == null)
             return;
         
         if (circleScaleValue < audioPower/2)
         {
-            circleScaleValue += Time.deltaTime * 2;
+            circleScaleValue += Time.deltaTime * audioPower * 0.2f;
             transform.localScale = new Vector3(circleScaleValue, circleScaleValue, circleScaleValue);
         }
         
         else Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject)
+        {
+            Debug.Log($"Player ({other.gameObject.name}) find at {other.transform.position}");
+        }
     }
 }
