db44305b1c357404384b5c9be83c1c5b87fb532c
Asemerald <63583036+Asemerald@users.noreply.github.com>
2025-01-22 11:47:17 +0100

    ui

---
+++ Assets/Scripts/Player/PlayerController.Shooting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Screen = UnityEngine.Device.Screen;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Player
+{
+    public partial class PlayerController
+    {
+        private void HandleShooting()
+        {
+            Vector3 screenCenterPoint = new Vector3(Screen.width / 2f, Screen.height / 2f);
+            
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -76,6 +76,7 @@ namespace Player
         void Update()
         {
             HandleMovementUpdate();
+            HandleShooting();
             UpdateServerWithPredictionServerRpc(lastProcessedPosition, lastProcessedRotation);
         }
 
